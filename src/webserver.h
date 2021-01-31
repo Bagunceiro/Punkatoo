85c4bbf7c6a2b35395233cdf06dd0661cc04107f
Paul Abraham <paul@acasa.org.uk>
2021-01-31 18:10:18 -0300

    more cond comp

--- src/webserver.h
+++ src/webserver.h
@@ -1,12 +1,6 @@
 #pragma once
 
-//#ifdef ESP32
 #include <WebServer.h>
-
-//#else
-//#include <ESP8266WebServer.h>
-//#define WebServer ESP8266WebServer
-//#endif
 
 class FanConWebServer : public WebServer
 {
